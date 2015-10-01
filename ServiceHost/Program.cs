c077900f0b12b594abb3a9fecedd61be257807e5
yuliiasobol <yuliia.sobol@globallogic.com>
2015-10-01 19:58:31 +0300

    my changes
    my changes

---
+++ ServiceHost/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StoreServices;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace ServiceHost
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Type serviceType = typeof(EmailValidator);
+			//Uri serviceUri = new Uri("http://localhost:8733/Design_Time_Addresses/StoreServices/EmailValidator/");
+			//System.ServiceModel.ServiceHost host = new System.ServiceModel.ServiceHost(serviceType, serviceUri);
+			//host.Open();
+			//host.AddDefaultEndpoints();
+			//host.AddServiceEndpoint(new ServiceMetadataEndpoint());
+		}
+	}
+}
---
+++ StoreServices/EmailValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace StoreServices
+{
+	public class EmailValidator:IEmailValidator
+	{
+		public bool ValidateAddress(string emailAddress)
+		{
+			Console.WriteLine("Validating: {0}", emailAddress);
+
+			string pattern = @"^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@(([0-9a-zA-Z])+([-\w]*[0-9a-zA-Z])*\.)+[a-zA-Z]{2,9})$";
+			return Regex.IsMatch(emailAddress, pattern);
+		}
+	}
+}
