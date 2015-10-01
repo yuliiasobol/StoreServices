24eb8214a2ef9bddbe5557ace42af549f5e88ec9
yuliiasobol <yuliia.sobol@globallogic.com>
2015-10-01 23:03:38 +0300

    cstructure changes
    cstructure changes

---
+++ ServicesDTOLibrary/User'sDTO/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServicesDTOLibrary.User_sDTO
+{
+    public class User
+    {
+    }
+}
